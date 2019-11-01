82bfc664f9d90b5bee7e0837ddcf5362aabf930a
Mike Kipnis <mike.kipnis@gmail.com>
2019-11-01 15:59:39 -0400

    Update run_test.sh

--- Examples/InterestRateSwaps/run_test.sh
+++ Examples/InterestRateSwaps/run_test.sh
@@ -11,12 +11,12 @@ cleanup()
   kill $CONTRACT_PUBLISHER
   kill $MARKET_DATA_PUBLISHER_PID
 
-  kill $SWAPTION_SERVER_B_PID
-  kill $SWAPTION_SERVER_A_PID
+  kill $CALCULATOR_CURVE_B_PID
+  kill $CALCULATOR_CURVE_A_PID
 
-  kill $CLIENT_PID
+  kill $CLIENT_1_PID
+  kill $CLIENT_2_PID
 
-  kill $REPO_PID
   kill $REPO_PID
 
   echo "Done cleanup ... quitting."
@@ -28,32 +28,46 @@ CWD=`pwd`
 
 echo "Current working directory : " + $CWD
 
-COMMON_OPTS="-DCPSConfigFile dds_tcp_conf.ini -ORBInitRef NameService=iiop://localhost:23456/NameService"
-
 $DDS_ROOT/bin/DCPSInfoRepo -o repo.ior -ORBEndpoint iiop://localhost:12345&
 REPO_PID=$!
 
-$CWD/SwaptionServer $COMMON_OPTS -ORBLogFile swaptions_server1.log -ORBDebugLevel 10 -s SwaptionServer1 &
-SWAPTION_SERVER_A_PID=$!
+sleep 1
 
-$CWD/SwaptionServer $COMMON_OPTS -ORBLogFile swaptions_server2.log -ORBDebugLevel 10 -s SwaptionServer2 &
-SWAPTION_SERVER_B_PID=$!
+COMMON_OPTS="-DCPSConfigFile dds_tcp_conf.ini"
 
-$CWD/MarketDataPublisher $COMMON_OPTS  -ORBLogFile market_data_publisher.log -ORBDebugLevel 10&
+echo $COMMON_OPTS
+
+$CWD/IRSClient $COMMON_OPTS -i Client_1 -ORBLogFile client_1.log -ORBDebugLevel 1 &
+CLIENT_1_PID=$!
+
+$CWD/IRSClient $COMMON_OPTS -i Client_2 -ORBLogFile client_2.log -ORBDebugLevel 1 &
+CLIENT_2_PID=$!
+
+sleep 1
+
+$CWD/IRSCalculator $COMMON_OPTS  -n depot-fra-swap -c "Deposit/1M|Deposit/3M|FRA/6M9|FRA/6M12|Swap/2Y|Swap/5Y|Swap/10Y|Swap/15Y|Swap/50Y|" -ORBLogFile curve_a_calculator.log -ORBDebugLevel 10 &
+CALCULATOR_CURVE_A_PID=$!
+
+$CWD/IRSCalculator $COMMON_OPTS  -n depot-swap -c "Deposit/1M|Deposit/3M|Deposit/6M|Deposit/1Y|Swap/2Y|Swap/5Y|Swap/10Y|Swap/15Y|Swap/50Y|" -ORBLogFile curve_b_calculator.log -ORBDebugLevel 10 &
+CALCULATOR_CURVE_B_PID=$!
+
+sleep 1
+
+$CWD/MarketDataPublisher $COMMON_OPTS -ORBLogFile market_data_publisher.log -ORBDebugLevel 10 &
 MARKET_DATA_PUBLISHER_PID=$!
 
-sleep 5
-
-$CWD/Client $COMMON_OPTS -ORBDebugLevel 10  -ORBLogFile swaptions_client.log -c  "Deposit/1M|Deposit/3M|FRA/6M9|FRA/6M12|Swap/2Y|Swap/5Y|Swap/10Y|Swap/15Y|Swap/50Y|" &
-CLIENT_PID=$!
+$CWD/InstrumentDataPublisher $COMMON_OPTS -ORBLogFile contract_publisher.log -ORBDebugLevel 10 &
+CONTRACT_PUBLISHER=$!
 
 sleep 10
 
-kill $CLIENT_PID
+kill $CONTRACT_PUBLISHER
 kill $MARKET_DATA_PUBLISHER_PID
 
-kill $SWAPTION_SERVER_B_PID
-kill $SWAPTION_SERVER_A_PID
+kill $CALCULATOR_CURVE_B_PID
+kill $CALCULATOR_CURVE_A_PID
+
+kill $CLIENT_1_PID
+kill $CLIENT_2_PID
 
 kill $REPO_PID
-kill $NS_PID
